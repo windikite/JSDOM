057cfec462b14b0366a8d00f09763e9a4eada866
windikite <windikite@yahoo.com>
2024-09-18 15:34:45 -0400

    Update FormManipulation.js

--- FormManipulation.js
+++ FormManipulation.js
@@ -1,3 +1,17 @@
+// Assignment 2
+// Task2
+let contact_table = document.getElementById("contact_table");
+
+const contacts = [];
+
+function add_contact(contact) {
+     let name =
+          document.getElementById("fname").value +
+          " " +
+          document.getElementById("lname").value;
+
+     let new_contact = {};
+     new_contact.name = name;
      new_contact.email = document.getElementById("email").value;
      new_contact.message = document.getElementById("message").value;
      contacts.push(new_contact);
@@ -13,5 +27,3 @@ document.getElementById("submit").addEventListener("click", function (event) {
      clear_table(contact_table);
      add_contact();
      populate_table(contact_table, contacts);
-     // event.preventDefault();
-});
