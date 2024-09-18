6c070063c500003f0feba5231b68fbeee3d083b4
windikite <windikite@yahoo.com>
2024-09-18 15:38:14 -0400

    Create Styling.js

---
+++ Styling.js
@@ -0,0 +1,17 @@
+// Assignment 3
+// Task2
+function change_color(event, color) {
+     event.style.color = color;
+}
+
+function change_background(event, color) {
+     event.style.backgroundColor = color;
+}
+
+function change_border(event, color) {
+     event.style.borderColor = color;
+}
+
+function move(event, distance) {
+     event.style.marginLeft = "250px";
+}
